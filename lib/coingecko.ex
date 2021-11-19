0e5cd2e7def784ca399dfde68b5b5e26256b90a5
EVOKE <evoke.lektrique@gmail.com>
2021-11-20 00:17:20 +0330

    First commit

---
+++ lib/coingecko.ex
@@ -0,0 +1,8 @@
+defmodule Coingecko do
+  @moduledoc """
+  Documentation for `Coingecko`.
+  """
+
+  alias Coingecko.Behaviour
+  @behaviour Behaviour
+end
---
+++ lib/coingecko/behaviour.ex
@@ -0,0 +1,5 @@
+defmodule Coingecko.Behaviour do
+  @moduledoc """
+  A definition of the intended behavior of the core functions.
+  """
+end
